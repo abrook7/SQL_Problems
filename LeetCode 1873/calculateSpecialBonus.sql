60f15efd968ffd3912dbc24810f76e52c589ec22
abrook7 <94274659+abrook7@users.noreply.github.com>
2022-08-13 00:02:23 -0400

    Add files via upload
    My solution to problem 1873 using a Case statement.

---
+++ LeetCode 1873/calculateSpecialBonus.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT employee_id, 
+CASE
+    WHEN employee_id % 2 = 1 && SUBSTRING(name,1,1) != 'M' THEN salary
+    ELSE salary = 0
+END AS bonus
+FROM Employees
+ORDER BY employee_id
